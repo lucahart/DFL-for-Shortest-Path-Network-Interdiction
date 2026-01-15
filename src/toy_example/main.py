cd21577ce1ea97aac7693315ea39a3a6a3700664
Luca Hartmann <lhartmann@ucsd.edu>
2026-01-15 14:57:49 -0800

    Got dfl example running.

--- src/toy_example/main.py
+++ src/toy_example/main.py
@@ -58,62 +58,62 @@ def test_pfl_predictor():
     print(f"Predicted interdicted opt. sol.:\n{y_pred}")
     pass
 
-# def train_dfl_predictor(predictor, w_train, c_train, y_train, z_train, epochs=200, lr=1e-2):
-#     predictor.train()
-#     opt_model = ToyOptModel(None)
-#     criterion = pyepo.func.SPOPlus(opt_model, processes=1)
-#     optimizer_ = torch.optim.Adam(predictor.parameters(), lr=lr)
-#     for _ in range(epochs):
-#         optimizer_.zero_grad()
-#         c_pred = predictor(w_train)
-#         loss = criterion(c_pred, c_train, y_train, z_train, reduction="none")
-#         loss.backward()
-#         optimizer_.step()
-#     return predictor
+def train_dfl_predictor(predictor, w_train, c_train, y_train, z_train, epochs=200, lr=1e-2):
+    predictor.train()
+    opt_model = ToyOptModel(None)
+    criterion = pyepo.func.SPOPlus(opt_model, processes=1)
+    optimizer_ = torch.optim.Adam(predictor.parameters(), lr=lr)
+    for _ in range(epochs):
+        optimizer_.zero_grad()
+        c_pred = predictor(w_train)
+        loss = criterion(c_pred, c_train, y_train, z_train)
+        loss.backward()
+        optimizer_.step()
+    return predictor
 
-# def build_toy_dataset_dfl(w_values):
-#     c = feature_cost_mapping(w_values)
-#     y = optimizer(c)
-#     z = sol_value(c, y)
-#     return w_values, c, y, z
+def build_toy_dataset_dfl(w_values):
+    c = feature_cost_mapping(w_values)
+    y = optimizer(c)
+    z = sol_value(c, y)
+    return w_values, c, y, z
 
-# def toy_example_dfl():
-#     predictor = nn.Sequential(
-#         nn.Linear(1, 4),
-#         nn.ReLU(),
-#         nn.Linear(4, 4)
-#     )
-#     w_values = torch.tensor([-2.0, -1.0, 0.5, 1.0, 2.0, 5.0]).unsqueeze(-1)
-#     w_train, c_train, y_train, z_train = build_toy_dataset_dfl(w_values)
-#     train_dfl_predictor(predictor, w_train, c_train, y_train, z_train)
-#     return predictor
+def toy_example_dfl():
+    predictor = nn.Sequential(
+        nn.Linear(1, 4),
+        nn.ReLU(),
+        nn.Linear(4, 4)
+    )
+    w_values = torch.tensor([-2.0, -1.0, 0.5, 1.0, 2.0, 5.0]).unsqueeze(-1)
+    w_train, c_train, y_train, z_train = build_toy_dataset_dfl(w_values)
+    train_dfl_predictor(predictor, w_train, c_train, y_train, z_train)
+    return predictor
 
-# def test_dfl_predictor():
-#     pred_dfl = toy_example_dfl()
-#     w = torch.tensor([-1.5, 1.0, 3.0]).unsqueeze(-1) # test features
-#     c_true = feature_cost_mapping(w) # true test costs
-#     with torch.no_grad():
-#         c_pred = pred_dfl(w) # predicted test costs
-#     intd = interdictor(c_true) # interdictions
-#     c_intd_true = c_true + intd # true interdicted costs
-#     c_intd_pred = c_pred + intd # predicted interdicted costs
-#     y_true = optimizer(c_intd_true) # true interdicted opt. sol.
-#     y_pred = optimizer(c_intd_pred) # predicted interdicted opt. sol.
-#     print(f"True costs:\n{c_true}")
-#     print(f"Predicted costs:\n{c_pred}")
-#     print(f"Interdictions:\n{intd}")
-#     print(f"True interdicted costs:\n{c_intd_true}")
-#     print(f"Predicted interdicted costs:\n{c_intd_pred}")
-#     print(f"True interdicted opt. sol.:\n{y_true}")
-#     print(f"Predicted interdicted opt. sol.:\n{y_pred}")
-#     pass
+def test_dfl_predictor():
+    pred_dfl = toy_example_dfl()
+    w = torch.tensor([-1.5, 1.0, 3.0]).unsqueeze(-1) # test features
+    c_true = feature_cost_mapping(w) # true test costs
+    with torch.no_grad():
+        c_pred = pred_dfl(w) # predicted test costs
+    intd = interdictor(c_true) # interdictions
+    c_intd_true = c_true + intd # true interdicted costs
+    c_intd_pred = c_pred + intd # predicted interdicted costs
+    y_true = optimizer(c_intd_true) # true interdicted opt. sol.
+    y_pred = optimizer(c_intd_pred) # predicted interdicted opt. sol.
+    print(f"True costs:\n{c_true}")
+    print(f"Predicted costs:\n{c_pred}")
+    print(f"Interdictions:\n{intd}")
+    print(f"True interdicted costs:\n{c_intd_true}")
+    print(f"Predicted interdicted costs:\n{c_intd_pred}")
+    print(f"True interdicted opt. sol.:\n{y_true}")
+    print(f"Predicted interdicted opt. sol.:\n{y_pred}")
+    pass
 
 def train_adfl_predictor():
     pass
 
 def main():
-    test_pfl_predictor()
-    # test_dfl_predictor()
+    # test_pfl_predictor()
+    test_dfl_predictor()
 
 if __name__ == "__main__":
     main()
